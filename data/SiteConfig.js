e6bca591eab64e8368d7d21955e94327a00f3f59
fonty1 <mr.matto@gmail.com>
2018-02-10 19:24:41 +0200

    First commit for Thinking on Signals!

---
+++ data/SiteConfig.js
@@ -0,0 +1,40 @@
+module.exports = {
+  blogPostDir: "blog", // The name of directory that contains your posts.
+  siteTitle: "Thinking on Signals", // Site title.
+  siteTitleAlt: "Better thinking.", // Alternative site title for SEO.
+  siteLogo: "/logos/thinking.png", // Logo used for SEO and manifest.
+  siteUrl: "https://thinkingonsignals.com", // Domain of your website without pathPrefix.
+  // pathPrefix: "/gatsby-material-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
+  siteDescription: "Better thinking.", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml", // Path to the RSS file.
+  //siteFBAppID: "1825356251115265", // FB Application ID for using app insights
+  //siteGATrackingID: "UA-47311644-4", // Tracking code ID for google analytics.
+  //disqusShortname: "https-vagr9k-github-io-gatsby-material-starter", // Disqus shortname.
+  postDefaultCategoryID: "Thinking", // Default category for posts.
+  userName: "Fontana", // Username to display in the author segment.
+  userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "North Pole, Earth", // User location to display in the author segment.
+  userAvatar: "/logos/thinking.png", // User avatar to display in the author segment.
+  userDescription:
+    "Something.", // User description to display in the author segment.
+  // Links to social profiles/projects you want to display in the author segment/navigation bar.
+  userLinks: [
+    // {
+    //   label: "GitHub",
+    //   url: "https://github.com/Vagr9K/gatsby-material-starter",
+    //   iconClassName: "fa fa-github"
+    // },
+    // {
+    //   label: "Twitter",
+    //   url: "https://twitter.com/Vagr9K",
+    //   iconClassName: "fa fa-twitter"
+    // },
+    // {
+    //   label: "Email",
+    //   url: "mailto:vagr9k@gmail.com",
+    //   iconClassName: "fa fa-envelope"
+    // }
+  ],
+  copyright: "Copyright © 2018." // Copyright string for the footer of the website and RSS feed.
+};
